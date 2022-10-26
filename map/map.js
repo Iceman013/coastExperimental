e9e53943eab9ab10bcf6b2b359197e86f0f1e47d
Bret Jones <bretcj1@gmail.com>
2022-10-26 16:07:35 -0400

    Map motion

--- map/map.js
+++ map/map.js
@@ -1,5 +1,5 @@
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 10, bottom: 40, left: 100},
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
     width = 600 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
@@ -14,32 +14,31 @@ var svg = d3.select("svg")
 //var path = d3.geoPath();
 var projection = d3.geoMercator()
     .scale(70)
-    .center([0,20])
-    .translate([width / 2 - margin.left, height / 2]);
+    .center([0,0])
+    .translate([222, 201]);
 
 // Data and color scale
 var data = d3.map();
 
-var promises = []
-promises.push(d3.json("map.geojson"))
-
+var promises = [];
+promises.push(d3.json("map.geojson"));
 
 myDataPromises = Promise.all(promises).then(function(topo) {
-    var topo = topo[0]
+	var topo = topo[0]
 
-    // Draw the map
-  	svg.append("g")
-    	.selectAll("path")
-    	
-    	.data(topo.features)
-    	.enter()
-    	.append("path")
-    	.attr("class", "topo")
-      	// draw each country
-      	.attr("d", d3.geoPath()
-        	.projection(projection)
-      	)
-      	// set the color of each country
-      	.attr("fill", "rgb(255,0,0)")
-      	.style("opacity", .7)
-})
+	// Draw the map
+	svg.append("g")
+		.selectAll("path")
+		
+		.data(topo.features)
+		.enter()
+		.append("path")
+		.attr("class", "topo")
+		// draw each country
+		.attr("d", d3.geoPath()
+			.projection(projection)
+		)
+		// set the color of each country
+		.attr("fill", "rgb(255,0,0)")
+		.style("opacity", 1)
+});
---
+++ map/motion.js
@@ -0,0 +1,70 @@
+const MAGNIFICATION = 1.2;
+var magnify = 1;
+var currentx = 0;
+var currenty = 0;
+const PAINFIX = 0.168;
+
+function zoom(direction, x, y) {
+	if (LOG) {
+        console.time("Zoom");
+    }
+    if (direction > 0) {
+        currentx = currentx + PAINFIX*x/magnify;
+        currenty = currenty + PAINFIX*y/magnify;
+        magnify = magnify/MAGNIFICATION;
+    } else {
+        currentx = currentx - PAINFIX*x/magnify;
+        currenty = currenty - PAINFIX*y/magnify;
+        magnify = magnify*MAGNIFICATION;
+    }
+	svg.select("g")
+		.attr("transform", "scale(" + magnify + "), translate(" + currentx + "," + currenty + ")")
+        .selectAll("path")
+            .style("stroke-width", (1/magnify)*0.5 + "px")
+    if (LOG) {
+        console.timeEnd("Zoom");
+    }
+}
+
+var moving = false;
+var holdx = 0;
+var holdy = 0;
+
+function move(x, y) {
+    if (LOG) {
+        console.time("Move");
+    }
+    currentx = currentx + (x - holdx)/magnify;
+    currenty = currenty + (y - holdy)/magnify;
+    svg.select("g")
+		.attr("transform", "scale(" + magnify + "), translate(" + currentx + "," + currenty + ")")
+    if (LOG) {
+        console.timeEnd("Move");
+    }
+}
+
+document.getElementById("map").addEventListener("wheel", function(event) {
+    zoom(event.deltaY, event.offsetX, event.offsetY);
+});
+
+
+document.getElementById("map").addEventListener("mousedown", function(event) {
+    moving = true;
+    holdx = event.offsetX;
+    holdy = event.offsetY;
+});
+document.getElementById("map").addEventListener("mouseleave", function() {
+    moving = false;
+});
+document.getElementById("map").addEventListener("mouseup", function() {
+    moving = false;
+});
+document.getElementById("map").addEventListener("mousemove", function(event) {
+    if (moving) {
+        if (holdx != event.offsetX && holdy != event.offsetY) {
+            move(event.offsetX, event.offsetY);
+            holdx = event.offsetX;
+            holdy = event.offsetY;
+        }
+    }
+});
