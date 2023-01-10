9058f967dafd2cb8b36609ea651fb7a7e6dbb5be
Bret Jones <bretcj1@gmail.com>
2023-01-10 15:34:33 -0500

    I tried

--- leaflet/main.js
+++ leaflet/main.js
@@ -3,3 +3,11 @@ var map = L.map('map').setView([33.9480, -83.3773], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+/*
+var imageUrl = '../Ian_Tampa_Test_250.jpg',
+    imageBounds = [[33.9480, -83.3773], [40.773941, -74.12544]];
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+*/
+
+// Create map
+var layer = L.leafletGeotiff("../Ian_Tampa_Test_250.tif", null).addTo(map);
