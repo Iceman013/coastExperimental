ee461bdf4f454fd1468f143e4e4f200cfe2308e8
Bret Jones <bretcj1@gmail.com>
2023-02-14 15:59:18 -0500

    More examples

--- afterMeet/js/tiffList.js
+++ afterMeet/js/tiffList.js
@@ -1,4 +1,7 @@
 var tiffList = [
-    new Tiff("Tiff 1", "https://uga-coast.s3.us-east-2.amazonaws.com/test/ian_cog.tif"),
-    new Tiff("Tiff 2", "https://uga-coast.s3.us-east-2.amazonaws.com/test/GA_COG_Test2.tif"),
+    new Tiff("Savannah", "https://uga-coast.s3.us-east-2.amazonaws.com/test/ian_cog.tif"),
+    new Tiff("Tiny Savannah", "https://uga-coast.s3.us-east-2.amazonaws.com/test/GA_COG_Test2.tif"),
+    new Tiff("Michael 1", "https://uga-coast.s3.us-east-2.amazonaws.com/example_cog/NGOM_Michael_GAHM_NAM_maxele_cog.tif"),
+    new Tiff("Michael 2", "https://uga-coast.s3.us-east-2.amazonaws.com/example_cog/NGOM_Michael_GAHM_NAM_HS_max_cog.tif"),
+    new Tiff("Michael 3", "https://uga-coast.s3.us-east-2.amazonaws.com/example_cog/EGOM_Ian_tau04_maxele_cog.tif"),
 ];
